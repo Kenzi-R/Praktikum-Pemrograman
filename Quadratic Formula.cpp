1adfae8fb02bcfcec228e33a87e3f253a0da7042
Kenzi-R <kenjiratanaputra@gmail.com>
2024-09-03 15:09:06 +0700

    Update Quadratic Formula.cpp

--- Quadratic Formula.cpp
+++ Quadratic Formula.cpp
@@ -2,20 +2,18 @@
 using namespace std;
 
     int main(){
-        //string s;cin>>s;
-    int a,b,c;
+    double a,b,c;
     cin>>a>>b>>c;
     int d=(b*b)-(4*a*c);
-    double sq=sqrt(d);
-    int x1,x2,x;
+    double x1,x2,x;
         if(d>0){
-        x1=((-b)+(sqrt(sq)))/(2*a);
-        x2=((-b)-(sqrt(sq)))/(2*a);
+        x1=((-b)+(sqrt(d)))/(2*a);
+        x2=((-b)-(sqrt(d)))/(2*a);
         cout<<"X1="<<x1<<endl;
         cout<<"X2="<<x2<<endl;
         }
         else if(d==0){
-            x=(b)/(2*a);
+            x=(-b)/(2*a);
             cout<<"X=";
             cout<<x;
         }
@@ -23,5 +21,4 @@ using namespace std;
             cout<<"No root Solution"<<endl;
             return 0;
         }
-        
     }
