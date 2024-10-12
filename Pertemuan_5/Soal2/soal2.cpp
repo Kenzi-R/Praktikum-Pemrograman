4f3c32f3c7c1eacb24a8593e25d14995131e1236
Kenji Ratanaputra <kenjiratanaputra@gmail.com>
2024-10-12 20:04:48 +0700

    Add files via upload

---
+++ Pertemuan_5/Soal2/soal2.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define pb push_back
+using namespace std;
+
+    int main(){
+        int n;cin>>n;
+        vector<int>v;
+        //Input nilai kedalam vector
+        while(n--){
+            int x;cin>>x;
+            v.pb(x);
+        }
+        int mx=v.size();
+        //Reversing array
+        for(int i=0;i<=mx/2;i++){
+            int temp=v[i];
+            v[i]=v[mx-i-1];
+            v[mx-i-1]=temp;
+        }
+        //Output semua isi dalam array
+        for(int i=0;i<mx;i++){
+            cout<<v[i]<<" ";
+        }
+    }
+
+// 0 1 2 3 4
+// 1 2 3 4 5
+
+// 9 5 6 7 3
